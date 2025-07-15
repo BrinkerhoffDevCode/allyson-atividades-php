7521210527a6df703ee36fa641f1bce8326864fc
Allyson <allyson_brinkerhoff@estudante.sesisenai.org.br>
2025-07-15 12:37:07 -0300

    update - revisao

--- lista-um/exercicio09.php
+++ lista-um/exercicio09.php
@@ -1,22 +1,31 @@
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>
-<?php
-    $num1 = '9';
-    $num2 = '6';
 
-    function somaNumeros($num1, $num2){
-        $soma = $num1 + $num2;
-        return $soma;
+<body>
+    <form method="POST" action="">
+        <label for="numero">Digite um número:</label>
+        <input type="number" id="numero" name="numero" required>
+        <button type="submit" name="verificar_numero_perfeito">Verificar</button>
+    </form>
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        if (isset($_POST['verificar_numero_perfeito'])) { {
+                $numero = $_POST['numero']; {
+                    $soma = $num1 + $num2;
+                    echo "$soma";
+                }
+            }
+        }
     }
 
-    $resultado = somaNumeros($num1, $num2);
     echo "A soma de $num1 e $num2 é: $resultado";
-?>
-    
+    ?>
+
 </body>
+
 </html>
--- lista-um/exercicio10.php
+++ lista-um/exercicio10.php
@@ -1,19 +1,33 @@
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>
-    <?php
-    function verificarPalindromo($palavra) {
-    $palavraInvertida = strrev($palavra);
-    return $palavra === $palavraInvertida;
-}
 
-    echo (verificarPalindromo("arara")); // true
-    // false
-?>
+<body>
+    <form method="POST" action="">
+        <label for="numero">Digite um número:</label>
+        <input type="number" id="numero" name="numero" required>
+        <button type="submit" name="palavra-invertida">Verificar</button>
+    </form>
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        if (isset($_POST['verificar_numero_perfeito'])) { {
+                $palavra = $_POST['numero']; { {
+                        $palavraInvertida = strrev($palavra);
+                        return $palavra === $palavraInvertida;
+                    }
+                }
+            }
+        }
+    }
+
+
+    echo"arara";
+
     ?>
 </body>
+
 </html>
