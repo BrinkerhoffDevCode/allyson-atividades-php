78d9c3a3d41ecd8b125b3a1f58a5d85e12755f80
Allyson Schaedler Brinkerhoff <allyson_brinkerhoff@estudante.sesisenai.org.br>
2025-08-25 20:14:33 -0300

    Update exercicio18.php

--- lista-um/exercicio18.php
+++ lista-um/exercicio18.php
@@ -1,28 +1,43 @@
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php
-    
 
-    $num1 = '7';
-    $num2 = '7';
-    $num3 = '7';
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Maior Número</title>
+    </head>
 
-    function qualNumeroMaior($num1, $num2, $num3) {
-        if ($num1 > $num2 && $num1 > $num3) {
-            return $num1;
-        } elseif ($num2 > $num1 && $num2 > $num3) {
-            return $num2;
-        } else {
-            return $num3;
-        }
-    }
-    ?>
+    <body>
 
-  
-</body>
-</html>
+        <form method="POST" action="">
+            <label for="numero1">Informe o 1° Número :</label>
+            <input type="number" id="numero1" name="numero1" required>
+            <br>
+            <label for="numero2">Informe o 2° Número :</label>
+            <input type="number" id="numero2" name="numero2" required>
+            <br>
+            <label for="numero3">Informe o 2° Número :</label>
+            <input type="number" id="numero3" name="numero3" required>
+            <br>
+            <button type="submit" name="verificar">Verificar</button>
+        </form>
+
+        <?php
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+            if (isset($_POST['verificar']));
+            $numero1 = $_POST['numero1'];
+            $numero2 = $_POST['numero2'];
+            $numero3 = $_POST['numero3'];
+            if ($numero1 > $numero2 and $numero1 > $numero3) {
+                echo ($numero1 . " É maior");
+            } else if ($numero2 > $numero1 and $numero2 > $numero3) {
+                echo ($numero2 . " É maior");
+            } else {
+                echo ($numero3 . " É maior");
+            };
+        };
+        ?>
+
+    </body>
+
+    </html>
