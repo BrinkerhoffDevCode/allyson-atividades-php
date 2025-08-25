f3336e8d8b370d4a39afa475b51e126c3985c080
Allyson Schaedler Brinkerhoff <allyson_brinkerhoff@estudante.sesisenai.org.br>
2025-08-25 20:13:52 -0300

    Update exercicio16.php

--- lista-um/exercicio16.php
+++ lista-um/exercicio16.php
@@ -1,24 +1,42 @@
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php
-    $dia = '10';
-    $mes = '07';
-    $ano = '2025';
-    
-    function validarData($dia, $mes, $ano){
-        if (checkdate($mes, $dia, $ano)){
-            return "A data $dia/$mes/$ano é valida";
-        } else {
-            return "A data $dia/$mes/$ano é invalida";
-        }
-        
-    }
-    ?>
-    
-</body>
-</html>
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Fibonacci</title>
+    </head>
+
+    <body>
+
+        <form method="POST" action="">
+            <label for="numero">Informe sua idade :</label>
+            <input type="number" id="numero" name="numero" required>
+            <button type="submit" name="verificar">Verificar</button>
+        </form>
+
+        <form method="POST1" action="">
+            <label for="text">Informe seu nome :</label>
+            <input type="text" id="text" name="text" required>
+            <button type="submit" name="verificar1">Verificar</button>
+        </form>
+
+        <?php
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+            if (isset($_POST['verificar']));
+            $numero = $_POST['numero'];
+            
+            if (isset($_POST1['verificar1']));
+            $text = $_POST1['text'];
+
+            if ($numero < 16){
+                echo ($text . " não pode votar ainda");
+            }else{
+                echo ($text . " pode votar ainda");
+            }
+        };
+        ?>
+
+    </body>
+
+    </html>
