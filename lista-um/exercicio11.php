b520cafe12c6d1a8b26d4cd104939b8ac0efc2e3
Allyson Schaedler Brinkerhoff <allyson_brinkerhoff@estudante.sesisenai.org.br>
2025-08-25 20:11:55 -0300

    Update exercicio11.php

--- lista-um/exercicio11.php
+++ lista-um/exercicio11.php
@@ -1,27 +1,42 @@
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Palíndromo</title>
 </head>
+
 <body>
+
     <form method="POST" action="">
-        <label for="numero">Digite um número:</label>
-        <input type="number" id="numero" name="numero" required>
-        <button type="submit" name="quantidade_vogais">Verificar</button>
+        <label for="text">Informe uma Palavra :</label>
+        <input type="text" id="text" name="text" required>
+        <button type="submit" name="verificar">Verificar</button>
     </form>
+
     <?php
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        if (isset($_POST['quantidade_vogais'])) { 
-                $palavra = $_POST['numero']; {
-                echo "A palavra digitada é: $palavra <br>"; 
-                echo "A quantidade de vogais é: " . 
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+        if (isset($_POST['verificar']));
+        $text = $_POST['text'];
 
 
-                }
+        function Palindromo($text)
+        {
+            //tive que pesquisar a fórmula para fazer o palíndromo , sorry
+            $reverso = strrev($text);
+            // strrev é para retornar a palavra invertida, pelo que vi
+            return $text == $reverso;
         }
-    }
+
+        if (Palindromo($text)) {
+            echo "$text é um palíndromo.\n";
+        } else {
+            echo "$text não é um palíndromo.\n";
+        }
+    };
     ?>
-    
+
 </body>
+
 </html>
