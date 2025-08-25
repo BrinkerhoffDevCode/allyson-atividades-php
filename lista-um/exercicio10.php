a4fed08d37146b9be2c8cd78637640c48c443f99
Allyson Schaedler Brinkerhoff <allyson_brinkerhoff@estudante.sesisenai.org.br>
2025-08-25 20:10:25 -0300

    Update exercicio10.php

--- lista-um/exercicio10.php
+++ lista-um/exercicio10.php
@@ -3,31 +3,31 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Fibonacci</title>
 </head>
 
 <body>
+
     <form method="POST" action="">
-        <label for="numero">Digite um número:</label>
+        <label for="numero">Informe um Número :</label>
         <input type="number" id="numero" name="numero" required>
-        <button type="submit" name="palavra-invertida">Verificar</button>
+        <button type="submit" name="verificar">Verificar</button>
     </form>
+
     <?php
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        if (isset($_POST['palavra-invertida'])) { {
-                $palavra = $_POST['numero']; { {
-                        $palavraInvertida = 'strrev'($palavra);
-                        echo $palavra === $palavraInvertida;
-                    }
-                }
-            }
-        }
-    }
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
+        if (isset($_POST['verificar']));
+        $numero = $_POST['numero'];
+        $count = 0;
 
-    echo"arara";
-
+        for ($i = 1; $i <= $numero; $i++) {
+            $count = $i + $count;
+        };
+        echo "<br>", $count;
+    };
     ?>
+
 </body>
 
 </html>
