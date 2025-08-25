dcc23ee192afbfb00fbcce0c25d8df479dcaa724
Allyson Schaedler Brinkerhoff <allyson_brinkerhoff@estudante.sesisenai.org.br>
2025-08-25 20:13:15 -0300

    Update exercicio14.php

--- lista-um/exercicio14.php
+++ lista-um/exercicio14.php
@@ -1,20 +1,35 @@
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Fibonacci</title>
 </head>
+
 <body>
+
+    <form method="POST" action="">
+        <label for="numero">Informe um ano :</label>
+        <input type="number" id="numero" name="numero" required>
+        <button type="submit" name="verificar">Verificar</button>
+    </form>
+
     <?php
-   
-   $altura = '1.75';
-   $peso = '70';
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-    function calcularIMC($peso, $altura) {
-         $imc = $peso / ($altura * $altura);
-         return round($imc, 2);
-    }
+        if (isset($_POST['verificar']));
+        $numero = $_POST['numero'];
+        
+        //Achei a fórmula no google , até pq já estava com dor de cabeça de fazer isto
+        if($numero % 4 == 0 AND ($numero % 100 != 0 OR $numero % 400 == 0)){
+            echo "Ano bixessto";
+        }else{
+            echo "Ano não bixessto";
+        }
 
+    };
     ?>
+
 </body>
+
 </html>
